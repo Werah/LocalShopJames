073acdfbc0898455812d6863cfa66b806a8747ad
Prof. James Campos <james.campos@etec.sp.gov.br>
2026-03-13 10:55:21 -0300

    Primiro commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './src/screens/Home/HomeScreen';
+import LojaDetalhes from './src/screens/Details/LojaDetalhes';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerTintColor: '#27ae60' }}>
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'LocalShop' }} 
+        />
+        <Stack.Screen 
+          name="Detalhes" 
+          component={LojaDetalhes} 
+          options={({ route }) => ({ title: route.params.loja.nome })} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
