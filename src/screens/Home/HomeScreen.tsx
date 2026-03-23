7f87f33576739f61e11225dfd0cac1a949614f1b
Your Name <your.email@example.com>
2026-03-23 11:39:02 -0300

    eu amo aprender

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,18 +1,23 @@
-import { View, Text, FlatList } from 'react-native';
+import { View, FlatList, ActivityIndicator } from 'react-native';
+import { useLojas } from '../../Hooks/useLojas';
 import LojaCard from '../../components/LojaCard';
-import { LOJAS } from '../../data/mockData';
-import { styles } from './StyleHome';
+
+//import { View, Text, FlatList } from 'react-native';
+//import LojaCard from '../../components/LojaCard';
+//import { LOJAS } from '../../data/mockData';
+//import { styles } from './StyleHome';
 
 export default function HomeScreen() {
+  const {lojas, loading} = useLojas();
+
+  if (loading) return <ActivityIndicator size="large" color="#27ae60" />;
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.headerTitle}>Lojas Próximas</Text>
+    <View style={{ flex: 1}}>
       <FlatList
-        data={LOJAS}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <LojaCard loja={item} />}
-        contentContainerStyle={styles.list}
-      />
+        data={lojas}
+        renderItem={({ item }) => <LojaCard loja={item}/>}
+    />
     </View>
-  );
+      );
 }
